5b70b5c41ccd66af37621d463ef176be9946940e
Florian Weiss <florian.weiss@weissheiten.at>
2018-04-27 15:05:12 +0200

    [TASK] Tests for Flow 5

---
+++ Build/src/DataTableGroupingPanel.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import "./scss/DataTableGroupingPanel.scss";
+
+class DataTableGroupingPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // expanded: false
+        }
+    }
+
+    render() {
+        return(
+            <div>DataTableGroupingPanel</div>
+        );
+    }
+}
+
+export default DataTableGroupingPanel;
